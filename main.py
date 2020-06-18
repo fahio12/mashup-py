8b6961a98260f4ad14e0fb15ea8df44ddc5e4fd2
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-18 12:08:56 -0400

    documentation and file structure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from spleeter.separator import Separator
+from spl import funk
 
 if __name__ == '__main__':
-    separator = Separator('spleeter:2stems')
-    separator.separate_to_file('./songs/army.mp3', './output')
+    funk.split(song="cradles")
---
+++ spl/funk.py
@@ -0,0 +1,17 @@
+from spleeter.separator import Separator
+
+
+def split(song,stems=2):
+    """Separates instruments from the song
+
+    Parameters:
+    song (string): name of the song
+    stems (int): number of instruments to split the song into
+
+    Output:
+    .wav files inside a folder named after the song. Files are named after the
+    instrument. Does not return anything
+
+    """
+    separator = Separator(f'spleeter:{stems}stems')
+    separator.separate_to_file(f'./songs/{song}.mp3', './output')
