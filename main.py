b8c892dd315aced131611a0b0f0995dfdd8897ad
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-19 11:44:49 -0400

    getFrames()

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from spl import funk
 
 if __name__ == '__main__':
-    funk.split(song="cradles")
+    # funk.split(song="cradles")
+    funk.getFrames(song = "army")
--- spl/funk.py
+++ spl/funk.py
@@ -1,5 +1,6 @@
 from spleeter.separator import Separator
-
+import librosa
+import numpy as np
 
 def split(song,stems=2):
     """Separates instruments from the song
@@ -14,4 +15,25 @@ def split(song,stems=2):
 
     """
     separator = Separator(f'spleeter:{stems}stems')
-    separator.separate_to_file(f'./songs/{song}.mp3', './output')
+    separator.separate_to_file(f'./songs/{song}.mp3', './output/instruments')
+
+def getFrames(song):
+    """Gets onset frames and times
+
+    Parameters:
+    song (string): name of the song
+
+    Output:
+    .wav song with the clicks for developing purposes
+    .csv file with timesptamps of the clicks
+    """
+    y, sr = librosa.load(f'./songs/{song}.mp3')
+    # #get frames
+    onset_frames = librosa.onset.onset_detect(y=y, sr=sr)
+    # #turn frames into time
+    frames_time = librosa.frames_to_time(onset_frames, sr=sr)
+    # #create clicks
+    clicks = librosa.clicks(frames_time, sr=sr, length=len(y))
+    #output
+    librosa.output.write_wav(f'./output/{song}-onset.wav', y + clicks, sr)
+    librosa.output.times_csv(f'./output/{song}-onsetimes.csv',frames_time)
