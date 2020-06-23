af39938062151a8bda928822ccb186e90f2f4082
Fabio Hernandez <44535804+fahio12@users.noreply.github.com>
2020-06-23 09:08:51 -0400

    gets fastest segments of the song

--- funk/segment.py
+++ funk/segment.py
@@ -1,24 +1,29 @@
 import csv
 
-def getFastSegment(frameTimes,seconds = 7):
+class segment:
+    def __init__(self,startTime,endTime,times):
+        self.startTime = startTime
+        self.endTime = endTime
+        self.startIndex = times.index(startTime)
+        self.endIndex = times.index(endTime)
+        self.frames = self.endIndex - self.startIndex
+
+def getFastSegment(frameTimes,seconds = 15):
     # round times list to 3 decimal places so its easier to work with
     times = []
     for time in frameTimes:
         times.append(round(time,3))
+
     # algorithm to get the most amount of frames in the time(seconds)
+    fastest = segment(times[0],times[0],times) #sets the fastest temp var (most amount of frames)
+    slowest = segment(times[0],times[len(times)-1],times) #sets the slowest temp var (least amount of frames)
     for i in range(len(times)):
         dinamicTimes = times[i:]
-        print("--------new--------")
         for j in range(len(dinamicTimes)):
-            time =  dinamicTimes[j] - times[i]
+            time = dinamicTimes[j] - times[i]
             if time >= seconds:
-                indexJ = times.index(dinamicTimes[j])
-                print(f"{dinamicTimes[j]} [{indexJ}] - {times[i]} [{i}] = {time}") #modify later
+                seg = segment(times[i],dinamicTimes[j],times)
+                if seg.frames > fastest.frames: #same concept for slowest segment jusut change the sign and vars
+                    fastest = seg
                 break
-    """
-    the algo gets all 7 second segments of the song with the starting
-    index and the ending index and the starting second and ending second
-    and lastly the time of the segment(around 7 seconds)
-    TODO: CALCULATE THE AMOUNT OF FRAMES OF EACH SEGMENT USING THE INDEXES OF
-    THE LIST AND RETURN THE SECONDS
-    """
+    print(f"{fastest.startTime} - {fastest.endTime} = {fastest.frames}")
